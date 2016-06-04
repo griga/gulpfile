dbb9f2fb69928681e61b16aea8d70fba8177aa21
Yura Griga <grigach@gmail.com>
2016-06-04 15:04:36 +0300

    updates

--- angular2-typescript/gulpfile.js
+++ angular2-typescript/gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const typescript = require('gulp-typescript');
 const connect = require('gulp-connect');
+const open = require('gulp-open');
 // const uglify = require('gulp-uglify')
 // const util = require('gulp-util')
 const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
 const tscConfig = require('./tsconfig.json');
+
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
 const inlineNg2Template = require('gulp-inline-ng2-template');
 
@@ -26,7 +30,9 @@ var sources = {
     sass: {
         main: 'src/sass/style.scss',
         src: [
+            'src/sass/style.scss',
             'src/sass/**/*.scss',
+            'src/app/**/*.scss',
         ]
     },
     css: [],
@@ -48,11 +54,12 @@ var sources = {
                 'node_modules/angular2/bundles/router.dev.js',
                 "node_modules/es6-shim/es6-shim.min.js",
                 "node_modules/angular2/bundles/router.dev.js",
+                "node_modules/angular2/bundles/http.dev.js",
             ]
     },
     assets: {
         src: [
-            'src/static/**/*.*'
+            'src/assets/**/*.*'
         ]
     },
     html: {
@@ -68,12 +75,15 @@ var destinations = {
         js: './.tmp/app',
         css: './.tmp/css',
         lib: './.tmp/lib',
+        assets: './.tmp/assets',
     },
     prod: {
         root: "./dist",
         js: './dist/js',
         lib: './dist/lib',
         css: './dist/css',
+        assets: './dist/assets',
+
     },
 };
 
@@ -82,8 +92,10 @@ gulp.task('ts', function () {
     return gulp
         .src(sources.ts.src)
         .pipe(inlineNg2Template({ base: './src',
-            target: 'es5',              // Can swap to es5
-            indent: 2,  }))
+            target: 'es5',
+            indent: 2,
+            useRelativePaths: true
+        }))
         .pipe(sourcemaps.init())
         .pipe(typescript(tscConfig.compilerOptions))
         .pipe(sourcemaps.write('.'))
@@ -93,15 +105,30 @@ gulp.task('ts', function () {
 
 gulp.task('watch', function () {
     gulp.watch(sources.ts.src, ['ts']);
+    gulp.watch(sources.ts.html, ['ts']);
     gulp.watch(sources.html.main, ['copy:main']);
+    gulp.watch(sources.sass.src, ['sass']);
 });
 
+gulp.task('sass', function () {
+    return gulp.src(sources.sass.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+
+        .pipe(gulp.dest(destinations.dev.css))
+        .pipe(connect.reload())
+})
+
 gulp.task('connect', function () {
-    connect.server({
+    return connect.server({
         root: destinations.dev.root,
         port: 10090,
         livereload: true
     });
+});
+gulp.task('open', function () {
+    return gulp.src(__filename)
+        .pipe(open({uri: 'http://localhost:10090'}));
 });
 
 gulp.task('copy:libs', function () {
@@ -112,6 +139,10 @@ gulp.task('copy:main', function () {
     gulp.src(sources.html.main)
         .pipe(gulp.dest(destinations.dev.root))
 })
+gulp.task('copy:assets', function () {
+    gulp.src(sources.assets.src)
+        .pipe(gulp.dest(destinations.dev.assets))
+})
 
 gulp.task('clean:dev', function(cb){
     return del([
@@ -121,4 +152,4 @@ gulp.task('clean:dev', function(cb){
 })
 
 
-gulp.task('default', ['ts', 'clean:dev', 'copy:libs', 'copy:main', 'watch',  'connect']);
+gulp.task('default', ['ts', 'sass', 'clean:dev', 'copy:libs', 'copy:main', 'copy:assets', 'watch',  'connect', 'open']);
